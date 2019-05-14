9bea81f468f63c6ae9b85f9b7f2f2e62dc39c34f
Robert O'Gara <rgo594@gmail.com>
2019-05-13 21:38:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+ function writeCards(array, events){
+   let card = []
+   for (let i = 0; i < array.length; i++){
+        card.push(`Thank you, ${array[i]}, for the wonderful ${events} gift!`)
+   }
+   return card
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function countdown(number) {
+  let i = 0; // the initialization moved OUTSIDE the body of the loop!
+  while (i < number) {
+    console.log(number);
+    number -= 1;
+     // the iteration move INSIDE the body of the loop!
+  }
+  console.log(number)
+}
